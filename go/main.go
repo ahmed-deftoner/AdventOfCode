e7957feb6b524d6cd9b159b9fa29afe547aae273
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-09-22 19:29:07 +0500

    file handling

--- go/main.go
+++ go/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"os"
+	"strconv"
+	"strings"
+)
 
+func ReadFile(fileName string) (nums []int, err error) {
+	body, err := os.ReadFile(fileName)
+	if err != nil {
+		return []int{}, err
+	}
+
+	Rawinput := strings.Split(string(body), "\n")
+	inputs := []int{}
+	for i := 0; i < len(Rawinput); i++ {
+		value := Rawinput[i]
+		if value == "" {
+			continue
+		}
+		num, err := strconv.Atoi(value)
+		if err != nil {
+			return []int{}, err
+		}
+		inputs = append(inputs, num)
+	}
+	return inputs, nil
+}
+
+func main() {
+	nums, err := ReadFile("E:/AdventOfCode/data.txt")
+	if err != nil {
+		println("failed to parse file")
+	}
+	println(len(nums))
 }

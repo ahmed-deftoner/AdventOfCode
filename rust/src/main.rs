8519d479fc968ba62505d1726bfd23ddbfacdf98
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-09-22 15:25:00 +0500

    print

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::Display;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error, ErrorKind, Read, self};
 
@@ -15,7 +16,27 @@ fn read(path : &str) -> Result<Vec<i64>, io::Error>{
     Ok(v)
 }
 
+
+struct NumVec(Vec<i64>);
+
+
+impl Display for NumVec {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let v = &self.0;
+        if v.len() == 0 {
+            return Ok(());
+        }
+        for num in &v[0..v.len() - 1] {
+            if let Err(e) = write!(f, "{}, ", &num.to_string()) {
+                return Err(e);
+            }
+        }
+        write!(f, "{}", &v[v.len() - 1])
+    }
+}
+
 fn main() {
-    let mut vec = read("../../data.txt").unwrap();
-    print!("{vec}");
+    let mut numbers = NumVec(Vec::new());
+    numbers.0 = read("/mnt/e/AdventOfCode/data.txt").unwrap();
+    print!("{numbers}");
 }

7d57503364e6a2c10a545d23e93c2b1241ce7e69
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-12-01 17:57:07 +0500

    1a fix

--- aoc22/src/main.rs
+++ aoc22/src/main.rs
@@ -1,17 +1,25 @@
+use std::borrow::BorrowMut;
+
 fn main() {
     let mut sums: Vec<u32> = Vec::new();
     let mut idx: usize = 0;
+    let mut max: u32 = 0;
+    let mut max_idx: usize = 0;
     sums.push(0);
     let input: Vec<&str> = include_str!("../data1.txt")
         .lines()
         .collect();
     for i in input {
         if i == "" {
+            if sums[idx] > max {
+                max = *sums[idx].borrow_mut();
+                max_idx = idx;
+            }
             sums.push(0);
             idx = idx + 1;
         } else {
             sums[idx] += i.parse::<u32>().unwrap();
         }
     }
-    println!("{:?}", sums.iter().max().unwrap());
+    println!("{:?}", max_idx + 1);
 }

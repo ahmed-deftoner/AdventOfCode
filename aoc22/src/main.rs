119f74a9d8b52c435a482930252d586eae5dd65b
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-12-01 18:01:16 +0500

    1a improve

--- aoc22/src/main.rs
+++ aoc22/src/main.rs
@@ -4,7 +4,6 @@ fn main() {
     let mut sums: Vec<u32> = Vec::new();
     let mut idx: usize = 0;
     let mut max: u32 = 0;
-    let mut max_idx: usize = 0;
     sums.push(0);
     let input: Vec<&str> = include_str!("../data1.txt")
         .lines()
@@ -13,7 +12,6 @@ fn main() {
         if i == "" {
             if sums[idx] > max {
                 max = *sums[idx].borrow_mut();
-                max_idx = idx;
             }
             sums.push(0);
             idx = idx + 1;
@@ -21,5 +19,5 @@ fn main() {
             sums[idx] += i.parse::<u32>().unwrap();
         }
     }
-    println!("{:?}", max_idx + 1);
+    println!("{:?}", max);
 }

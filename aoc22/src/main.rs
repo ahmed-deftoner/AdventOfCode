922e31569e68c719439dab9ae5e101d99f78a03e
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-12-03 17:09:18 +0500

    refactor

--- aoc22/src/main.rs
+++ aoc22/src/main.rs
@@ -1,5 +1,29 @@
 use std::borrow::BorrowMut;
 
+fn handle1() {
+    let mut sums: Vec<u32> = Vec::new();
+    let mut idx: usize = 0;
+    let mut max: u32 = 0;
+    sums.push(0);
+    let input: Vec<&str> = include_str!("../data1.txt")
+        .lines()
+        .collect();
+    for i in input {
+        if i == "" {
+            if sums[idx] > max {
+                max = *sums[idx].borrow_mut();
+            }
+            sums.push(0);
+            idx = idx + 1;
+        } else {
+            sums[idx] += i.parse::<u32>().unwrap();
+        }
+    }
+    sums.sort();
+    let total: u32 = sums.pop().unwrap() + sums.pop().unwrap() + sums.pop().unwrap();
+    println!("{:?}", total);
+}
+
 fn main() {
     let mut sums: Vec<u32> = Vec::new();
     let mut idx: usize = 0;

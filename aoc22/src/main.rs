1866ee3ed11151048abc9d3297792ccccd07791f
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-12-03 18:04:45 +0500

    got it right

--- aoc22/src/main.rs
+++ aoc22/src/main.rs
@@ -28,25 +28,29 @@ fn handle1() {
 fn main() {
     let mut op: Vec<u8> = Vec::new();
     let mut my: Vec<u8> = Vec::new();
+    let mut total: u32 = 0;
     let input: Vec<&str> = include_str!("../data1.txt")
         .lines()
         .collect();
     for line in input {
         let (opp, myy) = line.split_once(" ").unwrap();
-        let valop: u8 = match opp {
+        let valop: u32 = match opp {
             "A" => 1,
             "B" => 2,
             "C" => 3,
             _ => unreachable!()
         };
-        let valmy: u8 = match myy {
+        let valmy: u32 = match myy {
             "X" => 1,
             "Y" => 2,
             "Z" => 3,
             _ => unreachable!()
         };
-        op.push(valop);
-        my.push(valmy);
+        match valop.cmp(&valmy) {
+            std::cmp::Ordering::Less => total = total + 6 + valmy,
+            std::cmp::Ordering::Equal => total = total + valop + valmy,
+            std::cmp::Ordering::Greater => total = total + valmy,
+        }
     }
-    println!("{:?}", op);
+    println!("{:?}", total);
 }

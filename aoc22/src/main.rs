7bead6c04b9406f5809c7281e55d734515ec8b0c
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-12-11 21:04:58 +0500

    tree parse

--- aoc22/src/main.rs
+++ aoc22/src/main.rs
@@ -6,17 +6,17 @@ enum FileType {
 }
 
 #[derive(Debug)]
-struct Node<'a> {
+struct Node {
     idx: usize,
-    name: &'a str,
+    name: String,
     parent: Option<usize>,
     children: Vec<usize>,
     size: u32,
     file_type:FileType
 }
 
-impl<'a> Node<'a> {
-    fn new(idx: usize, name: &'a str, size: u32, file_type: FileType) -> Self {
+impl Node {
+    fn new(idx: usize, name: String, size: u32, file_type: FileType) -> Self {
         Self { 
             idx: idx,
             name: name,
@@ -29,12 +29,12 @@ impl<'a> Node<'a> {
 }
 
 #[derive(Debug,Default)]
-pub struct Tree<'a> {
-    tree: Vec<Node<'a>>
+pub struct Tree {
+    tree: Vec<Node>
 }
 
-impl<'a> Tree<'a> {
-    fn add_node(&mut self, name: &'a str, size: u32, file_type: FileType) -> usize {
+impl Tree {
+    fn add_node(&mut self, name: String, size: u32, file_type: FileType) -> usize {
         for node in &self.tree  {
             if node.name == name {
                 return node.idx;
@@ -52,21 +52,44 @@ fn main() {
         .skip(2)
         .collect();
     let mut tree: Tree = Tree::default();
-    let root = tree.add_node("/", 0, FileType::Directory);
+    let root = tree.add_node("/".to_owned(), 0, FileType::Directory);
     let mut pwd = root;
     let mut current = root;
     for line in &input {
         let cmd: Vec<&str> = line.split_whitespace().collect();
         match cmd[0] {
             "dir" => {
-                let dir = tree.add_node(cmd[1], 0, FileType::Directory);
+                let dir = tree.add_node( 
+                         format!("{}{}/", tree.tree[pwd].name.to_owned(), cmd[1].to_owned()),
+                    0, 
+                         FileType::Directory) ;
                 tree.tree[dir].parent = Some(pwd);
                 tree.tree[pwd].children.push(dir);
             },
             "$" => {
-
+                if cmd[1] == "cd" {
+                    if cmd[2] == ".." {
+                        current = tree.tree[current].parent.unwrap();
+                    } else {
+                        let dir = tree.add_node( 
+                            format!("{}{}/", tree.tree[pwd].name.to_owned(), cmd[1].to_owned()),
+                             0, 
+                             FileType::Directory);
+                        current = dir;
+                    }
+                    pwd = current;
+                }
             },
-            _ => ()
+            _ => {
+                let file_size = cmd[0].parse::<u32>().unwrap();
+                let file = tree.add_node(
+                    format!("{}{}", tree.tree[current].name, cmd[1]),
+                      file_size,
+                      FileType::File
+                    );
+                tree.tree[current].children.push(file);
+                tree.tree[file].parent = Some(current);
+            }
         }
     }
 }

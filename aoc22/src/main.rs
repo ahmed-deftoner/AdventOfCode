12fe8ad8ccff5870d09f9baed55103a52e7db4cb
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-12-11 20:06:25 +0500

    node

--- aoc22/src/main.rs
+++ aoc22/src/main.rs
@@ -1,56 +1,30 @@
-use std::{borrow::{BorrowMut}, collections::HashMap};
 
-
-pub struct Signal {
-    cycles: u32,
-    value: i32
+enum FileType {
+    Directory,
+    File
 }
 
-fn parse_10(input: &str) -> Vec<Signal> {
-    input
-        .lines()
-        .map(|line| line.split_whitespace())
-        .map(|mut line| {
-            let instruction = line.next().unwrap();
-            let value = match line.next() {
-                Some(value) => value.parse::<i32>().unwrap_or(0_i32),
-                None => 0_i32
-            };
-            match instruction {
-                "noop" => Signal { cycles: 1, value: value },
-                "addx" => Signal { cycles: 2, value: value },
-                _ => panic!("invalid instruction")
-            }
-        })
-        .collect()
+struct Node<'a> {
+    name: &'a str,
+    parent: Option<&'a str>,
+    children: Vec<&'a str>,
+    size: u32,
+    file_type:FileType
 }
+
+impl<'a> Node<'a> {
+    fn new(name: &'a str, size: u32, file_type: FileType) -> Self {
+        Self { 
+            name: name,
+            parent: None,
+            children: vec![],
+            size: size,
+            file_type: file_type
+        }
+    }    
+}
+
 
 fn main() {
-    let input: &str = include_str!("../data1.txt");
-    let mut crt: Vec<Vec<String>> = vec![vec![]; 6];
-    let mut pixel: i32 = 0;
-    let mut registry: i32 = 1;
-    let mut cycle = 0;
-    let mut row;
-    let signals = parse_10(input);
-
-    for signal in signals {
-        for _ in 0..signal.cycles {
-            row = cycle / 40;
-            cycle += 1;
-            if (registry - pixel).abs() <= 1 {
-                crt[row].push("#".to_owned());
-            } else {
-                crt[row].push(".".to_owned());
-            }
-            pixel += 1;
-            pixel %= 40;
-        }
-        registry += signal.value;
-    };
-
-    for row in crt {
-        println!("{}", row.join(""));
-    }
-    
+    let input = include_str!("../data1.txt");
 }
--- aoc22/src/sol/d1-10.rs
+++ aoc22/src/sol/d1-10.rs
@@ -1,3 +1,5 @@
+
+
 #[allow(dead_code)]
 fn handle1() {
     let mut sums: Vec<u32> = Vec::new();
@@ -305,4 +307,59 @@ fn handle9() {
 
     }
     println!("{:?}",total); */
+}
+
+pub struct Signal {
+    cycles: u32,
+    value: i32
+}
+
+fn parse_10(input: &str) -> Vec<Signal> {
+    input
+        .lines()
+        .map(|line| line.split_whitespace())
+        .map(|mut line| {
+            let instruction = line.next().unwrap();
+            let value = match line.next() {
+                Some(value) => value.parse::<i32>().unwrap_or(0_i32),
+                None => 0_i32
+            };
+            match instruction {
+                "noop" => Signal { cycles: 1, value: value },
+                "addx" => Signal { cycles: 2, value: value },
+                _ => panic!("invalid instruction")
+            }
+        })
+        .collect()
+}
+
+#[allow(dead_code)]
+fn handle10() {
+    let input: &str = include_str!("../data1.txt");
+    let mut crt: Vec<Vec<String>> = vec![vec![]; 6];
+    let mut pixel: i32 = 0;
+    let mut registry: i32 = 1;
+    let mut cycle = 0;
+    let mut row;
+    let signals = parse_10(input);
+
+    for signal in signals {
+        for _ in 0..signal.cycles {
+            row = cycle / 40;
+            cycle += 1;
+            if (registry - pixel).abs() <= 1 {
+                crt[row].push("#".to_owned());
+            } else {
+                crt[row].push(".".to_owned());
+            }
+            pixel += 1;
+            pixel %= 40;
+        }
+        registry += signal.value;
+    };
+
+    for row in crt {
+        println!("{}", row.join(""));
+    }
+     
 }

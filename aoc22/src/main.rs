ffa2504fb46447e432cf2beee832fc97a7a9415c
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-12-11 20:34:59 +0500

    idx

--- aoc22/src/main.rs
+++ aoc22/src/main.rs
@@ -7,6 +7,7 @@ enum FileType {
 
 #[derive(Debug)]
 struct Node<'a> {
+    idx: usize,
     name: &'a str,
     parent: Option<&'a str>,
     children: Vec<&'a str>,
@@ -15,8 +16,9 @@ struct Node<'a> {
 }
 
 impl<'a> Node<'a> {
-    fn new(name: &'a str, size: u32, file_type: FileType) -> Self {
+    fn new(idx: usize, name: &'a str, size: u32, file_type: FileType) -> Self {
         Self { 
+            idx: idx,
             name: name,
             parent: None,
             children: vec![],
@@ -32,14 +34,15 @@ pub struct Tree<'a> {
 }
 
 impl<'a> Tree<'a> {
-    fn add_node(&mut self, name: &'a str, size: u32, file_type: FileType) -> bool {
+    fn add_node(&mut self, name: &'a str, size: u32, file_type: FileType) -> usize {
         for node in &self.tree  {
             if node.name == name {
-                return false;
+                return node.idx;
             }
         }
-        self.tree.push(Node::new(name, size, file_type));
-        true
+        let idx = self.tree.len();
+        self.tree.push(Node::new(idx, name, size, file_type));
+        idx
     }
 }
 
@@ -50,6 +53,8 @@ fn main() {
         .collect();
     let mut tree: Tree = Tree::default();
     let root = tree.add_node("/", 0, FileType::Directory);
+    let mut pwd = root;
+    let mut current = root;
     for line in &input {
         let cmd: Vec<&str> = line.split_whitespace().collect();
         match cmd[0] {

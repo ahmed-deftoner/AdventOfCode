e87b4d8fbddcaeb0890832dec34104931d10ff33
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-12-12 23:33:04 +0500

    progress

--- aoc22/src/main.rs
+++ aoc22/src/main.rs
@@ -1,16 +1,47 @@
 
+
 #[derive(Debug)]
 struct Monkey {
     idx: usize,
     items: Vec<u32>,
-    worry: u32,
+    worry: (char, u32),
     test: u32,
     if_true: usize,
     if_false: usize
 }
 
+impl Monkey {
+    fn throw(&mut self) -> (Vec<usize>, Vec<u32>) {
+        let mut inspect: Vec<usize> = Vec::new();
+        let mut item: Vec<u32> = Vec::new();
+        for i in &self.items {
+            let mul = match self.worry.0 {
+                '*' => {
+                    match self.worry.1 {
+                        0 => (i * i) / 3,
+                        _ => (i * self.worry.1) / 3
+                    }
+                },
+                '+' => {
+                    match self.worry.1 {
+                        0 => (i + i) / 3,
+                        _ => (i + self.worry.1) / 3
+                    }
+                },
+                _ => unreachable!()
+            };
+            match mul % self.test {
+                0 => inspect.push(self.if_true),
+                _ => inspect.push(self.if_false)
+            };
+            item.push(mul);
+        }
+        (inspect, item)
+    }
+}
+
 fn main() {
-    let input: Vec<Monkey> = include_str!("../data1.txt")
+    let mut monke: Vec<Monkey> = include_str!("../data1.txt")
         .split("\n\n")
         .map(|line| {
             let mut x = line.trim_start()
@@ -31,14 +62,17 @@ fn main() {
                     .1
                     .split(", ")
                     .map(|x| {
-                        println!("{:?}",x);
                         x.parse::<u32>().unwrap()
                     })
                     .collect(), 
-                worry: x.next()
-                    .unwrap()
-                    .split_whitespace()
-                    .last().unwrap().parse::<u32>().unwrap_or(0),
+                worry: {
+                    let mut temp: Vec<&str> = x.next()
+                         .unwrap()
+                         .split_whitespace().collect();
+                    let x = temp.pop().unwrap().parse::<u32>().unwrap_or(0);
+                    let y = temp.pop().unwrap().parse::<char>().unwrap();
+                    (y, x)
+                },
                 test: x.next()
                     .unwrap()
                     .split_whitespace()
@@ -63,5 +97,14 @@ fn main() {
                 }
         })
         .collect();
-    println!("{:?}", input);
+    for _ in 0..2 {
+        for j in 0..monke.len() {
+            let (to, item) = monke[j].throw();
+            for k in 0..to.len() {
+                monke[j].items.clear();
+                monke[to[k]].items.push(item[k]);
+            }
+        }
+        println!("{:?}", monke[0].items);
+    }
 }

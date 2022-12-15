e57888e490d13ad1dc8e59e78b1f1a63e8fb64fc
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-12-15 23:22:00 +0500

    hash entry

--- aoc22/src/main.rs
+++ aoc22/src/main.rs
@@ -1,6 +1,9 @@
+use std::collections::{HashMap, HashSet};
+
 
 
 fn main() {
+    let mut cave: HashMap<u32, HashSet<u32>> = HashMap::new();
     let inp = include_str!("../data1.txt")
         .split("\n")
         .for_each(|line| {
@@ -10,6 +13,35 @@ fn main() {
                     println!("{:?} -> {:?}",x, y);
                     (x.parse::<u32>().unwrap(), y.parse::<u32>().unwrap())
                  })
-                 .collect::<Vec<(u32,u32)>>();
+                 .collect::<Vec<(u32,u32)>>()
+                 .windows(2)
+                 .into_iter()
+                 .for_each(|window| {
+                    let start = window[0];
+                    let end = window[1];
+                    if start.0 == end.0 {
+                        if end.1 > start.1 {
+                            (start.1..=end.1).for_each(|y| {
+                                cave.entry(start.0).or_default().insert(y);
+                            })
+                        } else {
+                            (end.1..=start.1).for_each(|y| {
+                                cave.entry(start.0).or_default().insert(y);
+                            })
+                        }
+                    } else if start.1 == end.1 {
+                        if start.0 > end.0 {
+                            (end.0..=start.0).for_each(|x| {
+                                cave.entry(x).or_default().insert(start.1);
+                                
+                            })
+                        } else {
+                            (start.0..=end.0).for_each(|x| {
+                                cave.entry(x).or_default().insert(start.1);
+                                
+                            })
+                        }
+                    }
+                 })
         });
 }

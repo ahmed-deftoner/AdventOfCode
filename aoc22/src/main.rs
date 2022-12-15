87ad8a2eb330c5555ffe9fcc6ee7c3fb71669029
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-12-15 23:04:56 +0500

    parse 14

--- aoc22/src/main.rs
+++ aoc22/src/main.rs
@@ -1,5 +1,15 @@
 
 
 fn main() {
-
+    let inp = include_str!("../data1.txt")
+        .split("\n")
+        .for_each(|line| {
+            line.split(" -> ")
+                .map(|pair| {
+                    let (x, y) = pair.split_once(",").unwrap();
+                    println!("{:?} -> {:?}",x, y);
+                    (x.parse::<u32>().unwrap(), y.parse::<u32>().unwrap())
+                 })
+                 .collect::<Vec<(u32,u32)>>();
+        });
 }

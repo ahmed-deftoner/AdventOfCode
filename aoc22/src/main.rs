9c2928addf8aa3c8f3738d7cca6f6adf2dc98023
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-12-17 23:58:14 +0500

    parse

--- aoc22/src/main.rs
+++ aoc22/src/main.rs
@@ -1,17 +1,45 @@
 use std::i32;
 
-
+#[derive(Debug)]
 struct Sensor {
     x: i32,
     y: i32,
     radius: i32
 }
 
+#[derive(Debug)]
 struct Beacon {
     x: i32,
     y: i32
 }
 
 fn main() {
-  
+    let inp: Vec<&str> = include_str!("../data1.txt")
+        .split("\n")
+        .map(|line| {
+            println!("{:?}", line);
+            let (s, b) = line.split_once(":").unwrap();
+            let (sx, sy) = s.split_once("Sensor at ")
+                .unwrap()
+                .1
+                .split_once(", ")
+                .unwrap();
+            let (bx, by) = b.split_once(" closest beacon is at ")
+                .unwrap()
+                .1
+                .split_once(", ")
+                .unwrap();
+            let beacon = Beacon {
+                x: bx.replace("x=", "").parse::<i32>().unwrap(),
+                y: by.replace("y=", "").parse::<i32>().unwrap(),
+            };
+            let sensor = Sensor {
+                x: sx.replace("x=", "").parse::<i32>().unwrap(),
+                y: sy.replace("y=", "").parse::<i32>().unwrap(),
+                radius: 1,
+            };
+            println!("{:?}", beacon);
+            sy
+        })
+        .collect();
 }
